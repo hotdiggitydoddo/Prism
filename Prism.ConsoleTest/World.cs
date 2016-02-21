d1cad10b8285650fcb3aaee04965b112c6cb1923
Ryan Dodd <ryan.dodd@identillect.com>
2016-02-20 22:37:25 -0800

    update ConsoleTest project

--- Prism.ConsoleTest/World.cs
+++ Prism.ConsoleTest/World.cs
@@ -15,39 +15,36 @@ namespace Prism.ConsoleTest
         Movable
     }
 
-    class World
+    class World : WorldBase
     {
-        private EntityManager _em;
-
-        public World(int maxEntities)
+        public World(int maxEntities) : base(maxEntities)
         {
-            _em = new EntityManager(maxEntities);
-            _em.RegisterComponentHandler<SpatialComponent>();
-            _em.RegisterComponentHandler<MovableComponent>();
+            EntityManager.RegisterComponentHandler<SpatialComponent>();
+            EntityManager.RegisterComponentHandler<MovableComponent>();
 
-            _em.AddSubsystem<PhysicsSubsystem>();
+            EntityManager.AddSubsystem<PhysicsSubsystem>();
 
-            var entity = _em.CreateEntity();
+            var entity = EntityManager.CreateEntity();
 
-            var spatial = _em.AddComponent<SpatialComponent>(entity);
+            var spatial = EntityManager.AddComponent<SpatialComponent>(entity);
             spatial.X = 5;
             spatial.Y = 10;
 
-            var movable = _em.AddComponent<MovableComponent>(entity);
+            var movable = EntityManager.AddComponent<MovableComponent>(entity);
             movable.VelX = .1f;
             movable.VelY = .1f;
 
-            entity = _em.CreateEntity();
+            entity = EntityManager.CreateEntity();
 
-            spatial = _em.AddComponent<SpatialComponent>(entity);
+            spatial = EntityManager.AddComponent<SpatialComponent>(entity);
             spatial.X = 52;
             spatial.Y = 22;
 
-            movable = _em.AddComponent<MovableComponent>(entity);
+            movable = EntityManager.AddComponent<MovableComponent>(entity);
             movable.VelX = .17f;
             movable.VelY = .17f;
 
-            _em.Update(.1666f);
+            EntityManager.Update(.1666f);
 
         }
     }
